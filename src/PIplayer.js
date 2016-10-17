454e1d907a6562e7b512e37c90c46491d3e6a504
justin.chang <justin.chang@quantatw.com>
2016-10-17 16:57:36 +0800

    Implement Jquery UI slider & button

--- src/PIplayer.js
+++ src/PIplayer.js
@@ -3,22 +3,22 @@ var piplayer;
 function PIplayer() {
 
     this.canvas = document.getElementById("videocanvas");
-	this.textcanvas = document.getElementById("textcanvas");
-	piplayer = new jsmpeg("./Videos/JHFQ9299.mpg", {
+    this.textcanvas = document.getElementById("textcanvas");
+    piplayer = new jsmpeg("./Videos/JHFQ9299.mpg", {
         canvas: this.canvas,
-		textcanvas: this.textcanvas,
+        textcanvas: this.textcanvas,
         autoplay: false,
         loop: false,
         seekable: true,
-		onload: PlayerOnLoad,
+        onload: PlayerOnLoad,
         ondecodeframe: DecodeReportProgress,
-		onfinished: FinishDecode
+        onfinished: FinishDecode
     });
-	
-	var btnPlay = document.getElementById("btnPlayVideo");
-    btnPlay.onclick = function(e) {
-        PlayOrPauseVideo();
-    };
+
+    /* var btnPlay = document.getElementById("btnPlayVideo"); */
+    // btnPlay.onclick = function(e) {
+    //     PlayOrPauseVideo();
+    /* }; */
 
 }
 
@@ -34,16 +34,55 @@ function PlayOrPauseVideo() {
 }
 
 function PlayerOnLoad() {
-	console.log("total duratopn %d s", piplayer.duration);
-   }
+	$("#timeSlider").slider("option", "min", 0);
+	$("#timeSlider").slider("option", "max", piplayer.duration);
+
+    console.log("total duratopn %d s", piplayer.duration);
+}
 
 function DecodeReportProgress() {
-	console.log("current time %d s" , piplayer.currentTime);
-	console.log("current frame %d / frame count %d" , piplayer.currentFrame, piplayer.frameCount);
+
+	$("#timeSlider").slider("option", "value", piplayer.currentTime);
+    console.log("current time %d s", piplayer.currentTime);
+    console.log("current frame %d / frame count %d", piplayer.currentFrame, piplayer.frameCount);
 
 }
 
 function FinishDecode() {
-	console.log("current time %d s" , piplayer.currentTime);
-	console.log("current frame %d / frame count %d" , piplayer.currentFrame, piplayer.frameCount);
+    console.log("current time %d s", piplayer.currentTime);
+    console.log("current frame %d / frame count %d", piplayer.currentFrame, piplayer.frameCount);
 }
+
+
+
+//
+//
+//
+// Jquery UI
+//
+//
+//
+
+$(document).ready(function(){
+	initSlider();
+	initPlayButton();
+});
+
+function initSlider() {
+   $("#timeSlider").slider({
+		animate: "fast",
+		change: function(event, ui) {
+			var target_second = ui.value;
+		}
+	});
+
+}
+
+function initPlayButton() {
+	$("#btnPlayVideo").button({});
+	$("#btnPlayVideo").click(PlayOrPauseVideo);
+}
+
+
+
+
